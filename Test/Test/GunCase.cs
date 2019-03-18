ae9db1a22e02076f652bd3b19bf0a9e7d00bf562
jsalliotte <40477668+jsalliotte@users.noreply.github.com>
2019-03-18 10:23:12 -0500

    Add files via upload

---
+++ Test/Test/GunCase.cs
@@ -0,0 +1,43 @@
+﻿using SolidWorks.Interop.swconst;
+using System.Runtime.InteropServices;
+using System;
+using SldWorks;
+class GunCase
+{
+
+
+    public void Main()
+    {
+
+        ModelDoc2 swModel;
+        SldWorks.SldWorks swApp = new SldWorks.SldWorks();
+        swModel = swApp.OpenDoc6("X:\\2.00 CLIENT PROJECTS\\DISPLAY SMART\\5' Display Case\\Actual\\DST-0100.sldasm", (int)swDocumentTypes_e.swDocASSEMBLY, (int)swOpenDocOptions_e.swOpenDocOptions_Silent, "", 1, 1);
+        swApp.Visible = true;
+
+
+        Dimension myDimension = default(Dimension);
+      
+        myDimension = (Dimension)swModel.Parameter("D1@OverallLength");
+
+        float metersToInches = 39.37f;
+        string end = "";
+        while (end != "E")
+        {
+           
+            Console.WriteLine("Enter Length");
+            float length = Convert.ToSingle(Console.ReadLine());
+
+            myDimension.SystemValue = length / metersToInches;
+          
+            swModel.ForceRebuild3(true);
+            Console.WriteLine("Enter E to exit or Press Any Key to enter new dimensions");
+            end = Console.ReadLine();
+
+
+        }
+
+        Console.ReadLine();
+
+    }
+
+}
---
+++ Test/Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using SolidWorks.Interop.swconst;
+using System.Runtime.InteropServices;
+using System;
+using SldWorks;
+namespace Add3_CSharp.csproj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GunCase GunCase = new GunCase();
+            BasicBox BasicBox = new BasicBox();
+
+            String fixtureType = "GunCase";
+
+            switch (fixtureType)
+            {
+                case "GunCase":
+                   
+                    GunCase.Main();
+                    break;
+                case "BasicBox":
+                   BasicBox.Main();
+                    break;
+                default:
+                    GunCase.Main();
+                    break;
+            }
+
+
+           
+
+            
+        }
+    }
+
+   
+
+}
